350f15487244ccb0be7b3f7cdd052b4870914d94
dk-github-acc <kankanwadidaneshwari55555@gmail.com>
2021-01-29 13:25:21 +0530

    Changed Version.hpp and created CTMacros.h

--- lib/include/public/Version.hpp
+++ lib/include/public/Version.hpp
@@ -12,19 +12,7 @@
 #ifndef RESOURCE_COMPILER_INVOKED
 #include <stdint.h>
 
-#ifdef HAVE_MAT_SHORT_NS
-#define MAT_NS_BEGIN  MAT
-#define MAT_NS_END
-#define PAL_NS_BEGIN  PAL
-#define PAL_NS_END
-#else
-#define MAT_NS_BEGIN  Microsoft { namespace Applications { namespace Events
-#define MAT_NS_END    }}
-#define MAT           ::Microsoft::Applications::Events
-#define PAL_NS_BEGIN  Microsoft { namespace Applications { namespace Events { namespace PlatformAbstraction
-#define PAL_NS_END    }}}
-#define PAL           ::Microsoft::Applications::Events::PlatformAbstraction
-#endif
+#include "CTMacros.h"
 
 #define MAT_v1        ::Microsoft::Applications::Telemetry
 
@@ -42,4 +30,3 @@ namespace PAL_NS_BEGIN { } PAL_NS_END
 
 #endif // RESOURCE_COMPILER_INVOKED
 #endif
-
